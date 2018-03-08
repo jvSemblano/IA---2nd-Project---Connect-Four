c6f381f68e97b02465f51b26941434beb518e80e
jvSemblano <36772001+jvSemblano@users.noreply.github.com>
2018-03-08 18:16:28 +0000

    Add files via upload

--- TRAB2.java
+++ TRAB2.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.lang.*;
 
 
-class Tabuleiro{
+/*class Tabuleiro{
 	private int[][] jogo;
 	private int value;
 
@@ -15,15 +15,148 @@ class Tabuleiro{
 		return jogo;
 	}
 
-}
+}*/
 
 public class TRAB2{
+
+
+	static int ler_Horizontal(int[][] matrix){
+		int countX = 0;
+		int countO = 0;
+
+		for(int i = 5; i >= 0; i--) {
+			for(int j = 0; j < 7; j++) {
+				if(matrix[i][j])=='X') {
+					countX++;
+					countO = 0;
+					if(countX == 4) {
+						//player wins
+						return 1;
+					}
+				}
+
+				else if(matrix[i][j]=='O') {
+					countO++;
+					countX = 0; 
+					if(countO == 4) {
+						//PC wins
+						return 2;
+					}
+				}	
+
+			}
+			countX = 0;
+			countO = 0;
+		}
+	}
+
+	static int ler_Vertical(int[][] matrix) {
+		int countX = 0;
+		int countO = 0;
+		for(int i = 0; i < 7; i++) {
+			for(int j = 5; j >= 0; j++) {
+				if(matrix[i][j])=='X') {
+				 	countX++;
+					countO = 0;
+					if(countX == 4) {
+						//player wins
+						return 1;
+					}
+				}
+
+				else if(matrix[i][j]=='O') {
+					countO++;
+					countX = 0; 
+					if(countO == 4) {
+						//PC wins
+						return 2;
+					}
+				}	
+
+			}
+		countX = 0;
+		countO = 0;
+
+		}
+	}
+	
+
+	static int ler_diagonal(int[][]matrix) {
+		int a=3;
+		int countX = 0;
+		int countO = 0;
+		for(int i=2; i>=0; i--) {
+			int k=i;
+			for(int j=0; j<=a; j++) {
+				if(matrix[k+1][j])=='X') {
+				 	countX++;
+					countO = 0;
+					if(countX == 4) {
+						//player wins
+						return 1;
+					}
+				}
+
+				else if(matrix[k+1][j]=='O') {
+					countO++;
+					countX = 0; 
+					if(countO == 4) {
+						//PC wins
+						return 2;
+					}
+				}	
+
+			}
+			countX = 0;
+			countO = 0;
+			a++;
+
+		}
+
+		a=5;
+		countX = 0;
+		countO = 0;
+
+		for(int i=1; i<=3; i++) {
+			for(int j=0; j<=a; j++) {
+				int k=i;
+				if(matrix[k+1][j])=='X') {
+				 	countX++;
+					countO = 0;
+					if(countX == 4) {
+						//player wins
+						return 1;
+					}
+				}
+
+				else if(matrix[k+1][j]=='O') {
+					countO++;
+					countX = 0; 
+					if(countO == 4) {
+						//PC wins
+						return 2;
+					}
+				}	
+
+			}
+			countX = 0;
+			countO = 0;
+			a--;
+
+
+
+			}
+
+		}
+	}
+	
+
+
 
     /*static Tabuleiro miniMax_Decision(int[][] jogo){
 	int v = MAX_VALUE(tabuleiro);
 	return 
     }
-
     static int MAX_VALUE(int[][] jogo){
 	if(TERMINAL_TEST(state)){
 	    return UTILITY(state);
@@ -33,10 +166,8 @@ public class TRAB2{
 	for(s : sucessores(state)){
 	    v = MAX(v, MIN_VALUE(s));
 	}
-
 	return v;	    
     }
-
     static int MIN_VALUE(state){
 	if(TERMINAL_TEST(state)){
 	    return UTILITY(state);
@@ -49,7 +180,7 @@ public class TRAB2{
 }*/
 
 
-	static Boolean makePlay(char[][] config, int coluna, int jogador){
+	static boolean makePlay(char[][] config, int coluna, int jogador){
 		if(coluna < 0 || coluna > 6){
 			System.out.println("Essa coluna nao existe! Jogada Impossivel...");
 			return false;
@@ -77,11 +208,11 @@ public class TRAB2{
 
 	public static void main(String[] args){
 		char[][] tabuleiro = {{'-','-','-','-','-','-','-'},
-				      {'-','-','-','-','-','-','-'},
-			              {'-','-','-','-','-','-','-'},
-			              {'-','-','-','-','-','-','-'},
-				      {'-','-','-','-','-','-','-'},
-				      {'-','-','-','-','-','-','-'}};
+				              {'-','-','-','-','-','-','-'},
+			                  {'-','-','-','-','-','-','-'},
+			                  {'-','-','-','-','-','-','-'},
+				              {'-','-','-','-','-','-','-'},
+				              {'-','-','-','-','-','-','-'}};
 		
 	}
 }
