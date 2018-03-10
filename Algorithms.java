0338f8e042164c9701e4ce14c3f34f80f470738f
jvSemblano <36772001+jvSemblano@users.noreply.github.com>
2018-03-10 23:04:02 +0000

    Add files via upload

--- Algorithms.java
+++ Algorithms.java
@@ -1,4 +1,6 @@
-static int miniMax(char[][] jogo){
+//MINIMAX
+
+static int mini_Max(char[][] jogo){
 	int v = max_Value(jogo);
 	return makeSuccessors(jogo, v);
 }
@@ -42,4 +44,50 @@ static int min_Value(char[][] jogo){
 
 static boolean terminal_Test(char[][] jogo){
 
+}
+
+
+//ALPHABET
+
+static int alpha_Beta(char[][] jogo){
+	int v = max_Value(jogo, Integer.MIN_VALUE , Integer.MAX_VALUE);
+	return makeSuccessors(jogo, v);
+}
+
+static int max_Value(char[][] jogo, alfa, beta){
+	if(terminal_Test(jogo)){
+		return 
+	}
+
+	int v = Integer.MIN_VALUE;
+
+	for(Tabuleiro s : makeSuccessors(jogo)){
+		v = Math.max(v, min_Value(s.getJogo(), alfa, beta));
+		if(v >= beta){
+			return v;
+		}
+
+		alfa = Math.max(alfa, v);
+	}
+
+	return v;
+}
+
+static int min_Value(char[][] jogo, alfa, beta){
+	if(terminal_Test(jogo)){
+		return 
+	}
+
+	int v = Integer.MAX_VALUE;
+
+	for(Tabuleiro s : makeSuccessors(jogo)){
+		v = Math.min(v, max_Value(s.getJogo(), alfa, beta));
+		if(v <= alfa){
+			return v; //momento da poda
+		}
+
+		beta = Math.min(beta, v);
+	}
+
+	return v;
 }
